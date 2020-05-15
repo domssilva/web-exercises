f779afffb409b3b90c1f9fbfb1c6bec7dd58edda
domssilva <silva@programmer.net>
2020-05-15 20:06:37 +0200

    spinal case conversion

--- strings/spinalCase.js
+++ strings/spinalCase.js
@@ -1,9 +1,29 @@
+/*
+  Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+  spinal case = this-is-spinal-case
+*/
+
 function spinalCase(str) {
-  let res = str.replace(/\W|_/gi, '-').toLowerCase();
-  console.log(res);
+  let char;
+  let strResult = '';
+  str = str.replace(/\s|_/gi, ' ');
+
+  for (let idx = 0; idx < str.length; idx++) {
+    char = str[idx];
+
+    if (!(char === char.toLowerCase()) && idx !== 0) {
+      strResult += ' ' + char.toLowerCase();
+    } else {
+      strResult += char.toLowerCase();
+    }
+  }
+
+  strResult = strResult.replace(/\s\s|\s/gi, '-');
+  console.log(strResult);
 }
 
-spinalCase("thisIsSpinalTap"); // how ???
-spinalCase('This Is Spinal Tap');
+spinalCase("This Is Spinal Tap");
+spinalCase("thisIsSpinalTap");
 spinalCase("The_Andy_Griffith_Show");
+spinalCase("Teletubbies say Eh-oh");
 spinalCase("AllThe-small Things");
