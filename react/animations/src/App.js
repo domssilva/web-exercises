8a0c1fb20b05904a4a692e6d7a56c795ad0ce30e
domssilva <silva@programmer.net>
2020-06-17 10:49:30 +0200

    react-spring: page transitions

--- react/animations/src/App.js
+++ react/animations/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Post from './components/post';
+import Post from './components/Post';
+import Slide from './components/Slide';
 import './App.css';
 
 function App() {
@@ -10,8 +11,7 @@ function App() {
         <a href="https://www.react-spring.io/docs/hooks/api" target="_blank">react-spring docs</a>
       </nav>
       <main>
-        <Post title='post 1'/>
-        <Post title='post 2'/>
+        <Slide/>
       </main>
     </div>
   );
--- react/animations/src/components/Post.jsx
+++ react/animations/src/components/Post.jsx
@@ -4,11 +4,17 @@ import {useSpring, animated} from 'react-spring'
 function Post(props) {
 
   // A spring simply animates values from one state to another.
-  const spring = useSpring({
+  const spring1 = useSpring({
     from: { color: 'red' },
     to: { color: 'blue' },
     delay: 1000,
     config: { duration: 500 },
+  });
+
+  const spring2 = useSpring({
+    from: { color: 'orange' },
+    to: { color: 'pink' },
+    delay: 2000,
   });
 
   const postCss = {
@@ -17,7 +23,7 @@ function Post(props) {
   };
 
   return (
-    <animated.div style={spring}>
+    <animated.div style={props.id === 1 ? spring1 : spring2}>
       <div style={postCss}>
         {props.title}
       </div>
