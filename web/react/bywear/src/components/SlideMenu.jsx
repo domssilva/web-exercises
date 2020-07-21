33fa559bc71e14be22f5f7ddf227057109a7f6c0
domssilva <silva@programmer.net>
2020-07-22 00:15:46 +0200

    [bywear] - fix routes ~ cart bug

--- web/react/bywear/src/components/SlideMenu.jsx
+++ web/react/bywear/src/components/SlideMenu.jsx
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom';
 
 const SlideMenu = ({version, cart, setCart, merchList}) => {
 
-  console.log(merchList);
+  let cartProduct = '';
 
-  
-
-  let cartProduct = Object.keys(cart).map(function(key) {
-    return (
-      <div className="cart__product">
-          {
-            merchList.map(obj => 
-              (obj.id === key) ? (
-                <>
-                  <div className="flex">
-                    <img className="cart__img" src={obj.img} alt="cart product"/>
-                    <p>{obj.name}</p>
-                  </div>
-                  <p>€ {obj.price}</p>
-                  <p>qtd: {cart[key]}</p>
-                </>
-              ): '')
-          }
-      </div>
-    )     
-  });
+  try {
+    cartProduct = Object.keys(cart).map(function(key) {
+      return (
+        <div className="cart__product">
+            {
+              merchList.map(obj => 
+                (obj.id === key) ? (
+                  <>
+                    <div className="flex">
+                      <img className="cart__img" src={obj.img} alt="cart product"/>
+                      <p>{obj.name}</p>
+                    </div>
+                    <p>€ {obj.price}</p>
+                    <p>qtd: {cart[key]}</p>
+                  </>
+                ): '')
+            }
+        </div>
+      )     
+    });
+  } catch(error) {
+    console.log(error);
+  }
 
   return (version === 'navigation' ) ?
     (
