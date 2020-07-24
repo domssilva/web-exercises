0c892b52841882f15b7568dfc96f57c679a2d708
domssilva <silva@programmer.net>
2020-07-24 15:26:37 +0200

    [bywear] - refactor UI

--- web/react/bywear/src/components/Header.jsx
+++ web/react/bywear/src/components/Header.jsx
@@ -12,22 +12,23 @@ const Header = () => {
     <div className='header__content'>
 
       <section className='header__social'>
-        <a href='#'>
+        <a href='https://twitter.com' target='_blank' rel='noopener noreferrer'>
           <svg className='header__social-icon'>
             <use xlinkHref={`${icons}#icon-twitter`}/>
           </svg>
         </a>
-        <a href='#'>
+        <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
           <svg className='header__social-icon'>
             <use xlinkHref={`${icons}#icon-instagram`}/>
           </svg>
         </a>
-        <a href='#'>
+        <a href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
           <svg className='header__social-icon'>
             <use xlinkHref={`${icons}#icon-facebook`}/>
           </svg>
         </a>
       </section>
+      <Link className='header__btn' to='/store'>Negozio</Link>
       <section className='header__main'>
         <h1 className='header__title'>
           <span className='orange'>Streetwear</span>
@@ -41,7 +42,6 @@ const Header = () => {
       </section>
     </div>
 
-    <Link className="btn btn--white" to="/store">Negozio</Link>
   </section>
   </>
   );
