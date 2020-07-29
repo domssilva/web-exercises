50bf8cf0bf0e315893a6076c361d52898a4cac86
domssilva <silva@programmer.net>
2020-07-29 15:01:35 +0200

    set product filters

--- web/react/bywear/src/components/Shop.jsx
+++ web/react/bywear/src/components/Shop.jsx
@@ -1,29 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import HeaderNav from './HeaderNav';
+import HeaderShop from './HeaderShop';
 import Product from './Product';
 
 const Shop = (props) => {
 
+    const [filters, setFilters] = useState('');
+
+    useEffect(() => {
+        console.log(filters);
+    }, [filters]);
+
+    let results;
+
+    if (filters === '') {
+        results = props.merch.map(({name, img, price, rating, id, description}) => (
+            <Product 
+                key={id}
+                name={name} 
+                img={img} 
+                id={id}
+                rating={rating}
+                price={price}
+                description={description}
+                cart={props.cart}
+                setCart={props.setCart}
+            />
+        ));
+    } else {
+        results = props.merch.map(({name, img, price, rating, id, description, category}) => {
+            if (category === filters) {
+                return (
+                    <Product 
+                        key={id}
+                        name={name} 
+                        img={img} 
+                        id={id}
+                        rating={rating}
+                        price={price}
+                        description={description}
+                        cart={props.cart}
+                        setCart={props.setCart}
+                    />
+                )
+            }
+        });
+    }
+
     return (
         <section className='content content-store'>
-            <HeaderNav/>
+            <HeaderShop
+                setFilters={setFilters}
+            />
             <div className="content-store__cell">
-                {
-                    props.merch.map(({name, img, price, rating, id, description}) => (
-                        <Product 
-                            key={id}
-                            name={name} 
-                            img={img} 
-                            id={id}
-                            rating={rating}
-                            price={price}
-                            description={description}
-                            cart={props.cart}
-                            setCart={props.setCart}
-                        />
-                    ))
-                }
+                { results }
             </div>
         </section >
     )
